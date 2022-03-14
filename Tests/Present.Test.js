c5cc2e9f00d6b02887695044a8e6c10b91f719df
SharkSV <SharkSV@outlook.com>
2022-03-14 21:32:51 +0200

    Updated Tests Changed tests to match the new behavior intended behavior

--- Tests/Present.Test.js
+++ Tests/Present.Test.js
@@ -1,11 +1,28 @@
-import Present from '../Lib/Present.js';
+import Present from '../Build/Present.js';
 
-Present({
-	title: 'Heyo',
-	tagLine: 'by @Shorky',
-	description: 'Just a test!',
-	version: '1.0',
-	fgColor: 'white',
-	bgColor: 'green',
-	clear: true
-});
+const hlpTxt = `
+Usage:
+	$ hyper <commands> [options]
+
+Commands:
+	init   Initialize a new project
+	list   List all available templates
+
+Options:
+	-h, --help  Show CLI help
+	-c, --clear  Clear Terminal
+	-v, --version  Show CLI version
+`;
+
+Present(
+	{
+		title: 'Hyper',
+		tagLine: 'by @Shorky',
+		description: 'A CLI to bootstrap new projects!',
+		version: '1.0',
+		fgColor: 'white',
+		bgColor: 'green',
+		clear: true
+	},
+	hlpTxt
+);
